8fcb3dbb0d7013bdbe124f1546c0ea049b8c8c2d
Daniel Magro <dando385@yahoo.com>
2023-11-08 12:19:22 -0500

    added top image

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import trollface from '../assets/images/troll-face.png';
+
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+                src={trollface} 
                 className="header--image"
+                alt="troll"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Funny Memes Only</h4>
         </header>
     )
 }
